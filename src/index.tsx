30d947bbc3544a57bb4778209f96a73a185f40df
codie <codiechanel@gmail.com>
2021-10-01 22:04:46 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,16 @@ import { render } from "solid-js/web";
 import { Router } from "solid-app-router";
 import App from "./app";
 // import "virtual:windi.css";
+
+// import { setup, strict, voidSheet } from "twind";
+// @ts-ignore
+import { setup } from "https://cdn.skypack.dev/twind/shim";
+import { Provider } from "./store"; // Tailwind V2 colors
 import "./index.css";
 import "./one-dark.css";
 
-import { setup, strict, voidSheet } from "twind";
-import * as colors from "twind/colors";
-import { Provider } from "./store"; // Tailwind V2 colors
-
 setup({
-  theme: {
-    extend: {
-      // @ts-ignore
-      gray: colors.trueGray,
-      coolgray: colors.coolGray,
-      colors: { hotpink: "#FF00FF" },
-    },
-  },
+  mode: "silent",
 });
 
 render(
